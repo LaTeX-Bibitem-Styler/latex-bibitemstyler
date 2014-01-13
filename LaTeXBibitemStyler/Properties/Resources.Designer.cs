3da606d9b8a2643eedf39a5eeb44ded93506ba47
pchiwan <pchiwan@gmail.com>
2014-01-13 22:12:57 +0000

    Update version and fix wrong text in label

--- LaTeXBibitemStyler/Properties/Resources.Designer.cs
+++ LaTeXBibitemStyler/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LaTeXBibitemStyler.Properties {
+namespace LaTeXBibitemsSorter.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LaTeXBibitemStyler.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LaTeXBibitemStyler.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LaTeXBibitemsSorter.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- LaTeXBibitemStyler/myForm.Designer.cs
+++ LaTeXBibitemStyler/myForm.Designer.cs
@@ -159,9 +159,9 @@ namespace LaTeXBibitemsStyler
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(12, 113);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.Size = new System.Drawing.Size(90, 17);
             this.label5.TabIndex = 1;
-            this.label5.Text = "Biblio Filename:";
+            this.label5.Text = "Output Style:";
             // 
             // label6
             // 
@@ -215,7 +215,7 @@ namespace LaTeXBibitemsStyler
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "myForm";
-            this.Text = "LaTeX Bibitem Styler by PchiwaN 2013";
+            this.Text = "LaTeX Bibitem Styler by PchiwaN 2014";
             this.Load += new System.EventHandler(this.myForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
