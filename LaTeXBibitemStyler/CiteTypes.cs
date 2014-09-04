0e1343b0470048d0825513b4c3fbad7f6a06f1f2
pchiwan <pchiwan@gmail.com>
2014-09-04 23:15:02 +0000

    Version 5.1 - Revised by suggestion of Oleg O. Kit (thanx!) ---- v5-01: add support for tags \citenum and \nocite v5-02: ignore commented out lines (those beginning with %) Also took the chance to implement big refactors/improvements

---
+++ LaTeXBibitemStyler/CiteTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LaTeXBibitemStyler
+{
+    public enum CiteTypes
+    {
+        /// <summary>
+        /// no tags found
+        /// </summary>
+        END = 0,
+
+        /// <summary>
+        /// \cite tag
+        /// </summary>
+        CITE = 1, 
+        
+        /// <summary>
+        /// \citenum tag
+        /// </summary>
+        CITENUM = 2, 
+        
+        /// <summary>
+        /// \nocite tag
+        /// </summary>
+        NOCITE = 3
+    }
+}
